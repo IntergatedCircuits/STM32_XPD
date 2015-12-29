0d0c978d77cc1d580e4b7ffc94f0f5e6d706d7d0
Benedek Kupper <kupper.benedek@gmail.com>
2015-12-29 02:19:23 +0100

    removed obsolete notice

--- CMSIS/Include/core_cmBitband.h
+++ CMSIS/Include/core_cmBitband.h
@@ -24,33 +24,6 @@
   *  You should have received a copy of the GNU General Public License
   *  along with STM32_XPD.  If not, see <http://www.gnu.org/licenses/>.
   */
-  /* ----------------------------------------------------------------------
- * Copyright (C) 2015 Benedek Kupper. All rights reserved.
- *
- * Date:        2015.11.17.
- * Revision:    v1.1
- *
- * Project:     STM32_XPD
- * Title:       core_cmBitband.h
- *
- * Description: This file provides macros and inline functions to
- * 				utilize bit-banding (implemented in Cortex M3 and up).
- *
- *  This file is part of STM32_XPD.
- *
- *  STM32_XPD is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  STM32_XPD is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
-
- *  You should have received a copy of the GNU General Public License
- *  along with STM32_XPD.  If not, see <http://www.gnu.org/licenses/>.
- * -------------------------------------------------------------------- */
 #ifndef CORE_CMBITBAND_H_
 #define CORE_CMBITBAND_H_
 
