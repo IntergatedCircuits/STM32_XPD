5d0862d087f4aead0bc0a88b6549e7d743e79829
Benedek Kupper <kupper.benedek@gmail.com>
2016-11-05 23:19:43 +0100

    Added file descriptions

--- STM32F0_XPD/inc/xpd_rcc_gen.h
+++ STM32F0_XPD/inc/xpd_rcc_gen.h
@@ -1,3 +1,26 @@
+/**
+  ******************************************************************************
+  * @file    xpd_rcc_gen.h
+  * @author  Benedek Kupper
+  * @version V0.1
+  * @date    2016-10-31
+  * @brief   STM32 eXtensible Peripheral Drivers RCC Peripherals Module
+  *
+  *  This file is part of STM32_XPD.
+  *
+  *  STM32_XPD is free software: you can redistribute it and/or modify
+  *  it under the terms of the GNU General Public License as published by
+  *  the Free Software Foundation, either version 3 of the License, or
+  *  (at your option) any later version.
+  *
+  *  STM32_XPD is distributed in the hope that it will be useful,
+  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  *  GNU General Public License for more details.
+  *
+  *  You should have received a copy of the GNU General Public License
+  *  along with STM32_XPD.  If not, see <http://www.gnu.org/licenses/>.
+  */
 #ifndef XPD_RCC_GEN_H_
 #define XPD_RCC_GEN_H_
 #include "xpd_rcc.h"
