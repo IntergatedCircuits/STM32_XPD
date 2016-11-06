cc9a13ab5ea89a4b7da3452d99b1b1f459fd3d55
Benedek Kupper <kupper.benedek@gmail.com>
2016-11-06 01:33:30 +0100

    Updated STM32F4 descriptors

--- CMSIS/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c
+++ CMSIS/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c
@@ -177,9 +177,9 @@ void SystemInit(void)
 
     /* Configure the Vector Table location add offset address ------------------*/
 #ifdef VECT_TAB_SRAM
-    SCB->VTOR = SRAM_BASE | VECT_TAB_OFFSET; /* Vector Table Relocation in Internal SRAM */
+    SCB->VTOR.w = SRAM_BASE | VECT_TAB_OFFSET; /* Vector Table Relocation in Internal SRAM */
 #else
-    SCB->VTOR = FLASH_BASE | VECT_TAB_OFFSET; /* Vector Table Relocation in Internal FLASH */
+    SCB->VTOR.w = FLASH_BASE | VECT_TAB_OFFSET; /* Vector Table Relocation in Internal FLASH */
 #endif
 
     /* initialize XPD services */
